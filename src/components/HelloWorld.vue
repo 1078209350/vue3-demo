d8e23ce65894ed1a4e4dad9ee052e11d0ee7ac8c
关童 <1078209350@qq.com>
2024-03-21 14:36:25 +0800

    [add]项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, useAttrs } from 'vue'
+
+//设置这个就不会默认把透传属性默认到根标签上了
+defineOptions({
+  inheritAttrs: false
+})
+
+const attrs = useAttrs()
+
+defineProps<{ msg: string }>()
+
+// const emit = defineEmits([ "doSth" ]);
+
+const count = ref(0)
+
+const handleButton = ()=>{
+  console.log(attrs)
+  attrs.onTest()
+}
+</script>
+
+<template>
+    <button type="button" @click="handleButton">attr: {{ $attrs }}</button>
+</template>
+
+<style scoped>
+</style>
